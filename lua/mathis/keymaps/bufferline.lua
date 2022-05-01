b97812ccdd7e40b9618de6cc44ba95d49f93d5e3
BenocxX <mathis.cote@outlook.com>
2022-05-01 00:45:22 -0400

    feat: add keymaps for Bufferline

--- lua/mathis/keymaps/bufferline.lua
+++ lua/mathis/keymaps/bufferline.lua
@@ -4,3 +4,6 @@ local k = vim.keymap
 
 k.set('n', 'H', ':BufferLineCyclePrev<cr>')
 k.set('n', 'L', ':BufferLineCycleNext<cr>')
+k.set('n', '<C-h>', ':BufferLineMovePrev<cr>')
+k.set('n', '<C-l>', ':BufferLineMoveNext<cr>')
+k.set('n', '<C-x>', ':bd<cr>')
