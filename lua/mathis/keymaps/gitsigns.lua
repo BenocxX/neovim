8785d7fd4eef1739758078953ac90febde37c069
BenocxX <mathis.cote@outlook.com>
2022-05-01 00:23:55 -0400

    feat: add keymaps

--- lua/mathis/keymaps/gitsigns.lua
+++ lua/mathis/keymaps/gitsigns.lua
@@ -4,3 +4,6 @@ local k = vim.keymap
 
 k.set('n', '<leader>gh', ':Gitsigns preview_hunk<cr>')
 k.set('n', '<leader>gr', ':Gitsigns reset_hunk<cr>')
+k.set('n', '<leader>gn', ':Gitsigns next_hunk<cr>')
+k.set('n', '<leader>gp', ':Gitsigns prev_hunk<cr>')
+k.set('n', '<leader>gb', ':Gitsigns blame_line<cr>')
