c1e7b0184e56bb659e5788bdebba3bb7b8813afa
BenocxX <mathis.cote@outlook.com>
2022-05-02 14:33:49 -0400

    refactor: update keymaps for lsp

--- lua/mathis/keymaps/lsp.lua
+++ lua/mathis/keymaps/lsp.lua
@@ -1,5 +1,4 @@
 -- Lsp keymaps
 local k = vim.keymap
 
-k.set('n', '<leader>l', ':LspInstallInfo<cr>1<cr><cr>', { silent = true })
-k.set('n', '<leader>lf', ':lua require("mathis.lsp.fixcurrent")()<cr>')
+k.set('n', '<leader>l', ':lua vim.lsp.buf.formatting_sync()<cr>')
