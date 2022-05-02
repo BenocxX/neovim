b2ce1539e72342e76a89fca00703d481fb906fa9
BenocxX <mathis.cote@outlook.com>
2022-05-02 14:32:24 -0400

    refactor: use protected call

--- lua/mathis/configurations/telescope.lua
+++ lua/mathis/configurations/telescope.lua
@@ -4,17 +4,17 @@ if not status_ok then
   return
 end
 
-require('telescope').setup {
+telescope.setup {
     defaults = {
         prompt_prefix = " ",
         selection_caret = " ",
-        path_display = { 
-            "smart" 
+        path_display = {
+            "smart"
         },
     },
     extensions = {
         media_files = {
-            filetypes = {"png", "webp", "jpg", "jpeg"}, 
+            filetypes = {"png", "webp", "jpg", "jpeg"},
             find_cmd = "rg"
         }
     },
