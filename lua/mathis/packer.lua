2df33633eec0907dc57d79325bbba7a2b0ec653c
BenocxX <mathis.cote@outlook.com>
2022-05-02 14:33:26 -0400

    feat: add Trouble plugin

--- lua/mathis/packer.lua
+++ lua/mathis/packer.lua
@@ -31,6 +31,17 @@ return require('packer').startup(function()
     -- LSP
     use 'neovim/nvim-lspconfig'
     use 'williamboman/nvim-lsp-installer'
+    use {
+        "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
 
     -- Navigation
     use {
